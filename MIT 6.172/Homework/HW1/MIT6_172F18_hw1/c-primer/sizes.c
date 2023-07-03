af741ee1e82ce72a79eef368c1832100bb1fe631
caixiongjiang <1837278011@qq.com>
2023-07-03 17:38:00 +0800

    update lecture pdf

---
+++ MIT 6.172/Homework/HW1/MIT6_172F18_hw1/c-primer/sizes.c
@@ -0,0 +1,43 @@
+// Copyright (c) 2012 MIT License by 6.172 Staff
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main() {
+  // Please print the sizes of the following types:
+  // int, short, long, char, float, double, unsigned int, long long
+  // uint8_t, uint16_t, uint32_t, and uint64_t, uint_fast8_t,
+  // uint_fast16_t, uintmax_t, intmax_t, __int128, and student
+
+  // Here's how to show the size of one type. See if you can define a macro
+  // to avoid copy pasting this code.
+  printf("size of %s : %zu bytes \n", "int", sizeof(int));
+  // e.g. PRINT_SIZE("int", int);
+  //      PRINT_SIZE("short", short);
+
+  // Alternatively, you can use stringification
+  // (https://gcc.gnu.org/onlinedocs/cpp/Stringification.html) so that
+  // you can write
+  // e.g. PRINT_SIZE(int);
+  //      PRINT_SIZE(short);
+
+  // Composite types have sizes too.
+  typedef struct {
+    int id;
+    int year;
+  } student;
+
+  student you;
+  you.id = 12345;
+  you.year = 4;
+
+
+  // Array declaration. Use your macro to print the size of this.
+  int x[5];
+
+  // You can just use your macro here instead: PRINT_SIZE("student", you);
+  printf("size of %s : %zu bytes \n", "student", sizeof(you));
+
+  return 0;
+}
